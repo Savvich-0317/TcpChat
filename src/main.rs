84d401d8b78e16152589f74e48911dc881774c3e
Savvich <savvaglinskij74@gmail.com>
2025-12-20 02:29:59 +0300

    major accidentantly created fully working 2 sided chat from first attempt! Lol i was actually surprized when it worked as i was thinkin, but i got sick and thats why it is so late

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
     net::TcpListener,
+    thread,
 };
 
 use crate::{listen::PrintStream, sender::TcpSender};
@@ -8,7 +9,7 @@ use crate::{listen::PrintStream, sender::TcpSender};
 mod listen;
 mod sender;
 fn main() {
-    println!("choose operation mode 2-sender 1-listener");
+    println!("choose operation mode 2-sender 1-listener 3 - client + server");
     let mut choose = "".to_string();
     io::stdin().read_line(&mut choose).unwrap();
 
@@ -16,13 +17,13 @@ fn main() {
         "1" => {
             let listener = TcpListener::bind("localhost:1212").unwrap();
 
-            for stream in listener.incoming() {
+            for mut stream in listener.incoming() {
                 println!("Got stream connection");
                 stream.unwrap().print_stream();
+                println!("connection closed");
             }
         }
         "2" => {
-            println!("choosed sender");
             let mut sender = TcpSender::new("localhost:1212".to_string(), 5).unwrap(); //drops stream if goes out of scope
 
             loop {
@@ -30,6 +31,25 @@ fn main() {
                 io::stdin().read_line(&mut message).unwrap();
                 sender.reply(message.to_string()).unwrap();
             }
+        }
+        "3" => {                                                                           //2121
+            let thread_listen = thread::spawn(||{let listener = TcpListener::bind("localhost:1212").unwrap();
+
+            for mut stream in listener.incoming() {
+                println!("Got stream connection");
+                stream.unwrap().print_stream();
+                println!("connection closed");
+            }});
+                                                                                          //1212
+            let thread_sender = thread::spawn(||{let mut sender = TcpSender::new("localhost:2121".to_string(), 5).unwrap(); //drops stream if goes out of scope
+
+            loop {
+                let mut message = "".to_string();
+                io::stdin().read_line(&mut message).unwrap();
+                sender.reply(message.to_string()).unwrap();
+            }});
+            thread_listen.join().unwrap();
+            thread_sender.join().unwrap();
         }
         &_ => {}
     }
